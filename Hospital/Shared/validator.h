fad9b837bbef399ae959dda112fc79fe0d68d756
shalevo <omrimexico@gmail.com>
2019-03-30 13:04:41 +0300

    fine tuning

--- Hospital/Shared/validator.h
+++
@@ -1,9 +0,0 @@
-#ifndef _VALIDATOR_H
-#define _VALIDATOR_H
-#include "../patient.h"
-
-bool isCharactersOnly(char *input, unsigned int length);
-bool isValidDate(Date date);
-bool isValidYear(int year);
-
-#endif //_VALIDATOR_H
